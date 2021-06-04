a0b78c99e95b227a4b5a90b4b120e203421fa408
Joe <joe44824@gmail.com>
2021-06-04 15:31:42 +0800

    Initial Commit

---
+++ BusTravel/BusTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTravelApp.swift
+//  BusTravel
+//
+//  Created by The Legend Joe on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BusTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
